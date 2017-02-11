dcd0358145cbed9e6170d4dc94e9329b07302ce8
AlexBischof <alexanderxbischof@gmail.com>
2017-02-11 13:14:42 +0100

    bug: change minutes to hours for hiwi hours

--- src/main/java/de/bischinger/buchungstool/jsf/BookingDto.java
+++ src/main/java/de/bischinger/buchungstool/jsf/BookingDto.java
@@ -4,70 +4,58 @@ import java.time.LocalTime;
 
 import static de.bischinger.buchungstool.business.TimeNumberListFunction.getLocalTime;
 
-public class BookingDto
-{
-	private String date;
-	private LocalTime start;
-	private LocalTime ende;
-	private int bruttoDuration;
-	private int nettoDuration;
+public class BookingDto {
+    private String date;
+    private LocalTime start;
+    private LocalTime ende;
+    private double bruttoDuration;
+    private double nettoDuration;
 
-	public BookingDto(String date, int from, int to, int nettoDuration, int bruttoDuration)
-	{
-		this.date = date;
-		this.start = getLocalTime(from);
-		this.ende = getLocalTime(to);
-		this.nettoDuration = nettoDuration;
-		this.bruttoDuration = bruttoDuration;
-	}
+    public BookingDto(String date, int from, int to, int nettoDuration, int bruttoDuration) {
+        this.date = date;
+        this.start = getLocalTime(from);
+        this.ende = getLocalTime(to);
+        this.nettoDuration = nettoDuration / 60d;
+        this.bruttoDuration = bruttoDuration / 60d;
+    }
 
-	public String getDate()
-	{
-		return date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public void setDate(String date)
-	{
-		this.date = date;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public LocalTime getStart()
-	{
-		return start;
-	}
+    public LocalTime getStart() {
+        return start;
+    }
 
-	public LocalTime getEnde()
-	{
-		return ende;
-	}
+    public LocalTime getEnde() {
+        return ende;
+    }
 
-	public void setStart(LocalTime start)
-	{
-		this.start = start;
-	}
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
 
-	public void setEnde(LocalTime ende)
-	{
-		this.ende = ende;
-	}
+    public void setEnde(LocalTime ende) {
+        this.ende = ende;
+    }
 
-	public int getBruttoDuration()
-	{
-		return bruttoDuration;
-	}
+    public double getBruttoDuration() {
+        return bruttoDuration;
+    }
 
-	public void setBruttoDuration(int bruttoDuration)
-	{
-		this.bruttoDuration = bruttoDuration;
-	}
+    public void setBruttoDuration(int bruttoDuration) {
+        this.bruttoDuration = bruttoDuration;
+    }
 
-	public int getNettoDuration()
-	{
-		return nettoDuration;
-	}
+    public double getNettoDuration() {
+        return nettoDuration;
+    }
 
-	public void setNettoDuration(int nettoDuration)
-	{
-		this.nettoDuration = nettoDuration;
-	}
+    public void setNettoDuration(int nettoDuration) {
+        this.nettoDuration = nettoDuration;
+    }
 }

11fbd288b526c9a7faa781768a4c0c0f5ec5b05a
AlexBischof <alexanderxbischof@gmail.com>
2017-02-14 23:21:03 +0100

    feat: add balkendiagramme

--- src/main/java/de/bischinger/buchungstool/jsf/HiwiDetailBean.java
+++ src/main/java/de/bischinger/buchungstool/jsf/HiwiDetailBean.java
@@ -1,5 +1,6 @@
 package de.bischinger.buchungstool.jsf;
 
+import de.bischinger.buchungstool.model.Booking;
 import de.bischinger.buchungstool.model.Hiwi;
 
 import javax.annotation.PostConstruct;
@@ -9,68 +10,142 @@ import javax.faces.bean.ManagedProperty;
 import javax.inject.Inject;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static java.util.Collections.emptyList;
+import static java.time.format.TextStyle.FULL;
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
+import static java.util.Locale.GERMAN;
+import static java.util.stream.Collectors.*;
+import static java.util.stream.IntStream.rangeClosed;
 
 /**
  * Created by Alex Bischof on 25.01.2017.
  */
 @ManagedBean
 @RequestScoped
-public class HiwiDetailBean
-{
-	@ManagedProperty(value = "#{param.name}")
-	private String name;
+public class HiwiDetailBean {
+    @ManagedProperty(value = "#{param.name}")
+    private String name;
 
-	private Hiwi hiwi;
+    private Hiwi hiwi;
 
-	@Inject
-	private HiwiListProcucer hiwiListProcucer;
+    @Inject
+    private HiwiListProcucer hiwiListProcucer;
 
-	@PostConstruct
-	public void init()
-	{
-		Optional<Hiwi> hiwiOptional = hiwiListProcucer.getHiwis().stream().filter(hiwi -> hiwi.getName().equals(name))
-				.findFirst();
-		if(hiwiOptional.isPresent())
-		{
-			this.hiwi = hiwiOptional.get();
-			this.name = hiwi.getName();
-		}
-		else
-		{
-			this.name = "Hiwi nicht vorhanden";
-		}
-	}
+    @PostConstruct
+    public void init() {
+        Optional<Hiwi> hiwiOptional = hiwiListProcucer.getHiwis().stream().filter(hiwi -> hiwi.getName().equals(name))
+                .findFirst();
+        if (hiwiOptional.isPresent()) {
+            this.hiwi = hiwiOptional.get();
+            this.name = hiwi.getName();
+        } else {
+            this.name = "Hiwi nicht vorhanden";
+        }
+    }
 
-	public String getName()
-	{
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public List<BookingDto> getBookings()
-	{
-		if (hiwi == null){
-			return emptyList();
-		}
-		DateTimeFormatter dateTimeFormatter = ofPattern("dd.MM.yyyy");
-		return hiwi.getScheduleMap().entrySet().stream()
-				.sorted(comparing(Entry::getKey))
-				.flatMap(e -> e.getValue().getBookingList().stream().map(b ->
-						new BookingDto(dateTimeFormatter.format(e.getKey()), b.getFrom(), b.getTo(), b.getNettoDuration(),
-								b.getBruttoDuration())
-				))
-				.collect(toList());
-	}
+    public String getBookingDates() {
+        if (hiwi == null) {
+            return "";
+        }
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
+        DateTimeFormatter dateTimeFormatter = ofPattern("dd.MM.yyyy");
+        return hiwi.getScheduleMap().keySet().stream().sorted()
+                .map(dateTimeFormatter::format)
+                .map(date -> "\"" + date + "\"")
+                .collect(joining(","));
+    }
+
+    public String getBookingValuesNetto() {
+        if (hiwi == null) {
+            return "";
+        }
+
+        return calculateValuesInternal(Booking::getNettoDuration);
+    }
+
+    public String getBookingValuesBrutto() {
+        if (hiwi == null) {
+            return "";
+        }
+        Function<Booking, Integer> getBruttoDuration = Booking::getBruttoDuration;
+
+        return calculateValuesInternal(getBruttoDuration);
+    }
+
+    public String getBookingValuesNettoWeeks() {
+        if (hiwi == null) {
+            return "";
+        }
+        return hiwi.getWeeklyNetto().keySet().stream().sorted().map(week -> "\"Week: " + week + "\"")
+                .collect(joining(","));
+    }
+
+    public String getBookingValuesNettoWeekValues() {
+        if (hiwi == null) {
+            return "";
+        }
+        return hiwi.getWeeklyNetto().entrySet().stream()
+                .sorted(comparing(Entry::getKey))
+                .map(Map.Entry::getValue)
+                .map(v -> "\"" + v + "\"")
+                .collect(joining(","));
+    }
+
+    public String getBookingValuesNettoMonthly() {
+        if (hiwi == null) {
+            return "";
+        }
+        return hiwi.getMonthlyNetto().keySet().stream().sorted()
+                .map(month -> month.getDisplayName(FULL, GERMAN))
+                .map(v -> "\"" + v + "\"")
+                .collect(joining(","));
+    }
+
+    public String getBookingValuesNettoMonthlyValues() {
+        if (hiwi == null) {
+            return "";
+        }
+        return hiwi.getMonthlyNetto().entrySet().stream()
+                .sorted(comparing(Entry::getKey))
+                .map(Map.Entry::getValue)
+                .map(v -> "\"" + v + "\"")
+                .collect(joining(","));
+    }
+
+    public String getBookingValuesPartialSums() {
+        if (hiwi == null) {
+            return "";
+        }
+        List<Double> dayBookingSums = hiwi.getScheduleMap().entrySet().stream()
+                .sorted(comparing(Entry::getKey))
+                .map(e -> e.getValue().getBookingList().stream().map(Booking::getNettoDuration).collect(summingInt(i -> i)))
+                .map(h -> h / 60d).collect(toList());
+        return rangeClosed(1, dayBookingSums.size())
+                .mapToObj(i -> dayBookingSums.subList(0, i).stream().collect(summingDouble(value -> value)))
+                .map(v -> "\"" + v + "\"")
+                .collect(joining(","));
+    }
+
+    private String calculateValuesInternal(Function<Booking, Integer> function) {
+        return hiwi.getScheduleMap().entrySet().stream()
+                .sorted(comparing(Entry::getKey))
+                .map(e -> e.getValue().getBookingList().stream().map(function).collect(summingInt(i -> i)))
+                .map(h -> h / 60d)
+                .map(date -> "\"" + date + "\"")
+                .collect(joining(","));
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }

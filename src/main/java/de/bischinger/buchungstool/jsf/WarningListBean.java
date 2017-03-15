80fb43a3d65d7f607068c5ea7c1482e93c20c866
alexander.bischof <ab@siteos.de>
2017-03-15 15:41:03 +0100

    bug: Gelöschte Warnungen im Kalender werden nicht gelöscht
     - SessionScope von WarningListBean entfernt

--- src/main/java/de/bischinger/buchungstool/jsf/WarningListBean.java
+++ src/main/java/de/bischinger/buchungstool/jsf/WarningListBean.java
@@ -4,7 +4,7 @@ import de.bischinger.buchungstool.business.WarningRepository;
 import de.bischinger.buchungstool.model.Warning;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -19,7 +19,7 @@ import static javax.enterprise.event.Reception.IF_EXISTS;
 /**
  * Created by Alex Bischof on 25.01.2017.
  */
-@SessionScoped
+@RequestScoped
 @Named
 public class WarningListBean implements Serializable {
     private static final long serialVersionUID = -5273419818689190540L;

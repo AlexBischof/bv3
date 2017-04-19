ccbf31a394d243cacbcce6eabba4ac46589d3b98
AlexBischof <alexanderxbischof@gmail.com>
2017-04-19 20:30:53 +0200

    chore: add mvn clean package to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+mvn clean package
+
 scp -P 28987 target/buchungstool.war bischofa@hosteurope:/home/bischofa/;
 ssh -t -p 28987 hosteurope "/opt/wildfly/bin/jboss-cli.sh --connect --controller=localhost:10090 --user=admin --password=test --command=\"deploy --force /home/bischofa/buchungstool.war\""
